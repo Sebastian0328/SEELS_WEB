415e29deebde90028e2bc5071676a4e8a49f3b80
sebastian <alvarezpaezsebastian@gmail.com>
2026-01-25 15:36:40 +0100

    agregado canonical a ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/servicio/services.component';
@@ -7,14 +8,36 @@ import { CoverageComponent } from './components/coverage/coverage.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent,HomeComponent,ServicesComponent,CoverageComponent,ReviewsComponent,FooterComponent],
+  imports: [
+    NavComponent,
+    HomeComponent,
+    ServicesComponent,
+    CoverageComponent,
+    ReviewsComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SeelsWeb';
+
+  constructor(@Inject(DOCUMENT) private doc: Document) {}
+
+  ngOnInit(): void {
+    const canonicalUrl = 'https://www.seels.online/';
+
+    let link = this.doc.querySelector("link[rel='canonical']") as HTMLLinkElement | null;
+
+    if (!link) {
+      link = this.doc.createElement('link');
+      link.setAttribute('rel', 'canonical');
+      this.doc.head.appendChild(link);
+    }
+
+    link.setAttribute('href', canonicalUrl);
+  }
 }
